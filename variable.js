7ed8f567f4f5fb649fb128186255309a42216c17
wj5w <wj5w>
2023-02-06 10:16:56 -0800

    initial commit

---
+++ variable.js
@@ -0,0 +1,50 @@
+//Variables store data for later use
+
+//Create a variable with let
+let myVariable = "value"
+let myNewVariable =7;
+let myBool = true;
+
+//Accessing variable values
+console.log(myVariable);
+console.log(myNewVariable+5);
+console.log(myBool && true);
+
+//Update value stored in a variable
+myNewVariable =10;
+console.log(myNewVariable);
+
+//Increase by 1
+myNewVariable=myNewVariable+1;
+console.log(myNewVariable);
+
+//Decrease by 1
+myNewVariable=myNewVariable-1;
+console.log(myNewVariable);
+
+//Multiply by 3
+myNewVariable = myNewVariable * 3;
+console.log(myNewVariable);
+
+//Divide by 3
+myNewVariable = myNewVariable /3;
+console.log(myNewVariable);
+
+//dive and return the remainder
+myNewVariable = MynewVariable %2 // 10/2=5 with 0 reminder
+console.log(myNewVariable);
+
+//Shortcuts!
+letx=100;
+x=x=1;//add 1
+x++;//add 1
+
+x=x-1//subtract 1
+x--;//subtract 1
+
+x=x-2;//subtract 2
+x-=2;//subtract 2
+
+x=x*10;//times 10
+x*=10;//times 10
+
